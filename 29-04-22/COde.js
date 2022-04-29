6869f69cd5f20905d14fa26b121f9f97ff385cb5
The-Danchan <104519456+The-Danchan@users.noreply.github.com>
2022-04-29 13:39:11 -0500

    bucles for 29-04-22

---
+++ 29-04-22/COde.js
@@ -0,0 +1,20 @@
+/* 
+        Bucle for
+        Determinado
+        su sintacsis consta de 3 partes
+        - variable
+        - numero de interaciones
+        - incrementación
+
+*/
+
+/* for(var i = 0; i<3; i++){
+    console.log(i);
+} */
+
+
+// ciclo for decremental
+
+for(var i = 10; i>0; i--){
+    console.log (i);
+}
---
+++ 29-04-22/CodeBucle.js
@@ -0,0 +1,23 @@
+// declaramos array
+var pokemones = ["Chorizord", "Venusaurio", "Blastoso"];
+// var i =  0;
+
+// while
+/* while(i<=2){
+    console.log(pokemones[i]);
+    i++;
+} */
+
+
+
+// do while
+/* do{
+   console.log (pokemones[i]);
+   i++;
+}while(i<=2); */
+
+
+
+for (var i = 0; i<=2; i++){
+    document.write(pokemones[i] + " yo te elijo mano <br>");
+}
